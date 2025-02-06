c19e65152242ac38f062d1030f929fdbcd06fb69
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-06 17:34:55 +0900

    [con]main

---
+++ TRPG/Dungeon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPG
+{
+    internal class Dungeon
+    {
+        public Dungeon()
+        {
+            Console.WriteLine("Dungeon created!");
+        }
+    }
+
+    public class Enemy
+    {
+        public Enemy()
+        {
+            Console.WriteLine("Dungeon created!");
+        }
+    }
+}
