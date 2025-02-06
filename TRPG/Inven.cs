c9494c69c969196ffea608704a51c14d367da018
Bosung <gobosung2000@gmail.com>
2025-02-06 20:53:42 +0900

    [Feat] item,Inven 기능 추가

--- TRPG/Inven.cs
+++ TRPG/Inven.cs
@@ -8,5 +8,37 @@ namespace TRPG
 {
     internal class Inven
     {
+        List<Item> items =new List<Item>();
+
+        /// <summary>
+        /// 스텟을 줄이고 + 가방에서 그 아이템 삭제
+        /// </summary>
+        /// <param name="itemName"></param>
+        public void ItemDelete(string itemName,Player player)
+        {
+            Item foundItem =items.Find(Item => Item.itemName == itemName);
+            //스텟을 감소시키기
+            StatusDecrease(foundItem,player);
+            //삭제
+            items.Remove(foundItem);
+        }
+
+        /// <summary>
+        /// 가방에 아이템 추가 
+        /// </summary>
+        /// <param name="_item"></param>
+        public void ItemAdd(Item _item)
+        {
+            items.Add(_item);
+        }
+        /// <summary>
+        /// 아이템에 대한 수치마큼 스텟에서 제외시켜줘
+        /// </summary>
+        /// <param name="foundItem"></param>
+        public void StatusDecrease(Item? foundItem,Player player)
+        {
+            //player의 스텟이 구성되면 그 구성된 수치에서 제외시켜주기 
+
+        }
     }
 }
--- TRPG/Item.cs
+++ TRPG/Item.cs
@@ -8,10 +8,41 @@ namespace TRPG
 {
     internal class Item : IStatus
     {
+        public Item(string _itemName, ItemType _itemType, int _hp, 
+            int _mp, int _atk, int _def)
+        {
+            this.itemName = _itemName;
+            this.itemType = _itemType;
+            ((IStatus)this).hp = _hp;
+            ((IStatus)this).mp = _mp;
+            ((IStatus)this).atk = _atk;
+            ((IStatus)this).def = _def;
+        }
+
+        public string itemName { get; }
+        public ItemType itemType { get; }
         int IStatus.hp { get; set; }
         int IStatus.mp { get; set; }
         int IStatus.atk { get; set; }
         int IStatus.def { get; set; }
+
+        /// <summary>
+        /// 아이템을 장착 메소드 
+        /// </summary>
+        public void EquipItem()
+        {
+
+        }
+
+        /// <summary>
+        /// 아이템을 해제하는 메소드
+        /// </summary>
+        public void ReleaseItem()
+        {
+
+        }
+
+
     }
 
     enum ItemType
