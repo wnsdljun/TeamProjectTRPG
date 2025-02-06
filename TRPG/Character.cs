b966ce5e132fc5c9b23fb60fb0f4d13c1d299dc3
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-06 17:43:15 +0900

    Suml

--- TRPG/Character.cs
+++ TRPG/Character.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TRPG
 {
-    internal class Charhcter
+    internal class Character
     {
     }
 }
---
+++ TRPG/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPG
+{
+    internal class Player
+    {
+        string name;
+        int level;
+        int exp;
+        int hp;
+        int mp;
+        int atk;
+        int def;
+        int gold;
+    }
+}
