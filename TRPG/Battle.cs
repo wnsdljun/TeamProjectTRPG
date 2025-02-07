7c7202a5398936a718d380c74d22a6dbc45e0400
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-07 11:14:38 +0900

    [feat]몬스터 스킬, 데미지

--- TRPG/Battle.cs
+++ TRPG/Battle.cs
@@ -8,5 +8,17 @@ namespace TRPG
 {
     internal class Battle
     {
+        private EnemyFactory enemyfactory = new EnemyFactory();
+        private Enemyskill enemyskill = new Enemyskill();
+
+   
+    }
+    internal class BattleSystem : Battle
+    {
+ 
+        public void BattleStart()
+        {
+           
+        }
     }
 }
--- TRPG/Enemy.cs
+++ TRPG/Enemy.cs
@@ -10,12 +10,13 @@ namespace TRPG
     {
         public string? name { get; set; }
         public int hp { get; set; }
-        public int mp { get ; set ; }
+        public int mp { get; set; }
         public int atk { get; set; }
-        public int def { get ; set; }
+        public int def { get; set; }
         public int gold { get; set; }//처치시 획득 골드
+        public int skillcode { get; set; }//처치시 획득 경험치
 
-        public Enemy(string Enemyname, int Hp, int Mp, int Atk, int Def, int Gold)
+        public Enemy(string Enemyname, int Hp, int Mp, int Atk, int Def, int Gold, int skillcode)
         {
             name = Enemyname;
             this.hp = Hp;
@@ -23,65 +24,55 @@ namespace TRPG
             this.atk = Atk;
             this.def = Def;
             this.gold = Gold;
+            this.skillcode = skillcode;
         }
     }
 
     internal class EnemyFactory
     {
-        Enemy MeleeMinion = new Enemy("전사 미니언",47,0,12,1,21);
-        Enemy CasterMinion = new Enemy("마법사 미니언", 29, 0, 24, 1, 14);
-        Enemy SuperMinion = new Enemy("슈퍼 미니언", 160, 0, 47, 10, 60);
+        Enemy MeleeMinion = new Enemy("전사 미니언", 47, 0, 12, 1, 21, 1);
+        Enemy CasterMinion = new Enemy("마법사 미니언", 29, 0, 24, 1, 14, 2);
+        Enemy SuperMinion = new Enemy("슈퍼 미니언", 160, 0, 47, 10, 60, 3);
+        Enemy TurretTower = new Enemy("포탑", 500, 0, 18, 5, 420, 11);
     }
 
-    //internal class MeleeMinion : Enemy
-    //{
-    //    public MeleeMinion()
-    //    {
-    //        name = "전사 미니언";
-    //        hp = 47;
-    //        mp = 0;
-    //        atk = 12;
-    //        def = 1;
-    //        gold = 21;
-    //    }
-    //}
-
-    //internal class CasterMinion : Enemy
-    //{
-    //    public CasterMinion()
-    //    {
-    //        name = "마법사 미니언";
-    //        hp = 29;
-    //        mp = 0;
-    //        atk = 24;
-    //        def = 1;
-    //        gold = 14;
-    //    }
-    //}
-
-    //internal class SuperMinion : Enemy
-    //{
-    //    public SuperMinion()
-    //    {
-    //        name = "슈퍼 미니언";
-    //        hp = 160;
-    //        mp = 5;
-    //        atk = 23;
-    //        def = 10;
-    //        gold = 60;
-    //    }
-    //}
-
-    //internal class TurretTower : Enemy
-    //{
-    //    public TurretTower()
-    //    {
-    //        name = "포탑";
-    //        hp = 500;
-    //        mp = 5;
-    //        atk = 18;
-    //        def = 5;
-    //        gold = 420;
-    //    }
-    //}
+    internal class Enemyskill
+    {
+        public int damage;
+        public void normalskill(Enemy enemy)
+        {
+            Console.WriteLine($"{enemy.name}의 공격!");
+            damage = enemy.atk;
+        }
+        public void OnlyTurretskill(Enemy enemy)
+        {
+            if (enemy.skillcode == 11)
+            {
+                if (enemy.mp >= 5)
+                {
+                    Console.WriteLine($"{enemy.name}의 강화 공격!");
+                    damage = enemy.atk * 2;
+                    Damage(enemy);
+                }
+                else
+                {
+                    Console.WriteLine($"{enemy.name}의 공격!");
+                    damage = enemy.atk;
+                    Damage(enemy);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{enemy.name}의 공격!");
+                damage = enemy.atk;
+                Damage(enemy);
+            }
+        }
+        public void Damage(Enemy enemy)
+        {
+            Random random = new Random();
+            int Randomdamage = random.Next(damage - (damage / 10), damage + (damage / 10 + 1));
+            Console.WriteLine($"플레이어를 {Randomdamage}의 피해를 입었다!");
+        }
+    }
 }
