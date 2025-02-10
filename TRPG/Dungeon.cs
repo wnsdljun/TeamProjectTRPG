858175719ae4353407888dd189b0b43737f9fd7e
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-10 09:25:04 +0900

    [Update] 던전 멘트

--- TRPG/Dungeon.cs
+++ TRPG/Dungeon.cs
@@ -13,8 +13,32 @@ namespace TRPG
             Console.WriteLine("협곡에 오신 것을 환영합니다." +
                 "\n앞으로 나아가시겠습니까?" +
                 "\n\n1. 전진하기" +
-                "\n2. 돌아가기");
+                "\n2. 나가기");
 
+        }
+        public void DungeonForward()
+        {
+            int input = 0;
+            Console.WriteLine("적이 곳 몰려옵니다. 어떻게 하시겠습니까?" +
+                "\n\n1. 전진하기" +
+                "\n2. 인벤토리" +
+                "\n3. 휴식하기" +
+                "\n4. 나가기");
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("적을 만났습니다. 전투를 시작합니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("인벤토리를 엽니다.");
+                    break;
+                case 3:
+                    Console.WriteLine("휴식을 취합니다.");
+                    break;
+                case 4:
+                    Console.WriteLine("협곡을 빠져나갑니다.");
+                    break;
+            }
         }
         public void DungeonEnd()
         {
